72b042666719410f1a21579209a8512a1d8a4f94
SatanLabs , Inc <46850618+satanlabs@users.noreply.github.com>
2020-05-06 11:11:15 +0530

    Update and rename node_npm_ng_install.md to node_npm_ng_install.sh

---
+++ node_npm_ng_install.sh
@@ -0,0 +1,14 @@
+# Install node and npm 
+
+$ sudo apt install nodejs
+
+$ sudo apt-get install npm
+
+#(ng will require node 10.+ )
+$ sudo npm install -g n
+$ sudo n stable
+
+//install angular ng
+$ npm install -g @angular/cli
+
+#https://askubuntu.com/questions/426750/how-can-i-update-my-nodejs-to-the-latest-version
