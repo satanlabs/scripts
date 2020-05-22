003e089061037ac7326503fbec1a1948bbbea843
SatanLabs , Inc <46850618+satanlabs@users.noreply.github.com>
2020-05-22 15:21:49 +0530

    Update python_csn_ml.sh

--- python_csn_ml.sh
+++ python_csn_ml.sh
@@ -4,7 +4,7 @@
 
 $ sudo apt-get update
 
-$ sudo apt-get install git httpie openjdk-8-jdk maven
+$ sudo apt-get install git httpie openjdk-8-jdk maven unzip
 
 #install pip3 and other things necessary
 
@@ -13,3 +13,5 @@ $ sudo apt-get -y install python3-pip
 $ sudo apt-get install unixodbc unixodbc-dev
 
 # https://github.com/mkleehammer/pyodbc/issues/252
+
+
