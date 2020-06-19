be06a32746777b1b65677813150ea78ab3c0e8ea
SatanLabs , Inc <46850618+satanlabs@users.noreply.github.com>
2020-06-19 18:25:32 +0530

    Create aws_cli_install.sh

---
+++ aws_cli_install.sh
@@ -0,0 +1,22 @@
+
+# go to AWS official page ( for more info ) : ( version 1 and 2 )
+# HERE we are installing Version-2.
+
+ $ curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+ 
+ $ unzip awscliv2.zip
+ 
+ $ sudo ./aws/install
+ 
+ $ aws --version
+ 
+ output : aws-cli/2.0.23 Python/3.7.3 Linux/5.3.0-1023-aws botocore/2.0.0dev27
+ 
+ $ aws configure
+ 
+ (follow steps )
+ 
+ $ aws s3 ls
+ ( to verify )
+ 
+ 
