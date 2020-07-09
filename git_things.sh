19443fde012f3cd7139ae54d8cb304314ec5eb2b
SatanLabs , Inc <46850618+satanlabs@users.noreply.github.com>
2020-07-09 22:11:28 +0530

    Update git_things.sh

--- git_things.sh
+++ git_things.sh
@@ -4,4 +4,23 @@
 # see history of a file ( in a  particular branch )
 $ git log -p <filename>
 
-
+###### conflict-resolving #######
+ Lets say we want to put code from Branch `feature-x` into the `dev` , and there's a conflict.
+ 
+ dev => master
+  
+ Then , 
+ 1. $ git pull
+ 
+ 2. git checkout feature-x
+ 
+ 3. git pull origin dev
+ 
+ 4. Resolve them conflicts.
+ 
+ 5. Push changes to `feature-x`.
+ 
+ #################################
+ 
+ 
+ 
