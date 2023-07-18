3f6a85d37075fcd56f177f9dd7bba0545f9c577d
SatanLabs, Inc <46850618+satanlabs@users.noreply.github.com>
2023-07-18 16:32:02 +0530

    Add files via upload

--- 20230718_startup_opener_mac.sh
+++ 20230718_startup_opener_mac.sh
@@ -1,27 +1,37 @@
 #!/bin/bash
 
+openAutomatically='YES'
 echo "Hello START ~ $(date)"
 
-userResponse='y'
+userResponseYes='y'
+userResponseNo='n'
 
 # arg-1 is common-name, arg-2 is the actual-name
 function openApp() {
 	#echo "arg1 ->$1<-"
 	#echo "arg2 ->$2<-"
-	printf "open $1 ? INPUT : y/n\n"
-	read -r shouldOpen
-	#echo "shouldOpen ->$shouldOpen<-"
+	shouldOpen=$userResponseNo
+	if [[ $openAutomatically = 'YES' ]]
+		then
+			shouldOpen=$userResponseYes
+			echo "shouldOpen ->$shouldOpen<-"
+		else
+			printf "open $1 ? INPUT : y/n\n"
+			read -r shouldOpen
+			echo "shouldOpen ->$shouldOpen<-"
+	fi
 
-	if [[ $shouldOpen = $userResponse ]]
+	if [[ $shouldOpen = $userResponseYes ]]
 		then 
 			open -a "/Applications/$2.app" && echo "opened $1"
-			#sleep 1
+			sleep 1
 		else
 			echo "okay, SKIPPED opening $1"
 	fi
 
 	printf "\n====~====~====~====~====~====~====~====\n"
 }
+
 
 openApp "Docker" "Docker"
 openApp "GitHub Desktop" "GitHub Desktop"
